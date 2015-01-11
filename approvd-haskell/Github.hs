c389a9b1740f5720863f5f833e15cb25c1fa362e
Georges Dubus <georges.dubus@compiletoi.net>
2015-01-11 21:47:21 +0100

    Put the user and the repo inside the Github monad

--- approvd-haskell/Github.hs
+++ approvd-haskell/Github.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving #-}
-module Github (Github, Token, runGithub, get, post) where
+module Github (Github, runGithub, get, post, getRepo, postRepo) where
 
 import Control.Applicative (Applicative)
 import Control.Lens ((^.), (&), (?~), (.~))
+import Control.Monad ((>=>))
 import Control.Monad.Trans (liftIO)
 import Control.Monad.Trans.Reader (ReaderT (..), ask)
 import qualified Data.ByteString as B
@@ -12,17 +13,21 @@ import qualified Network.Wreq as W
 import Network.Wreq.Types (Postable)
 
 
-type Token = B.ByteString
-
-data GithubConfig = GithubConfig { gToken :: B.ByteString }
+data GithubConfig = GithubConfig { gToken :: B.ByteString
+                                 , gUser :: String
+                                 , gRepo :: String }
 
 -- | A monad that call the github api with a given authentication
 newtype Github a = Github { runG :: ReaderT GithubConfig IO a }
                  deriving (Functor, Monad, Applicative)
 
 -- | Run Github with a given auth and extract the final value
-runGithub :: Token -> Github a -> IO a
-runGithub t g = runReaderT (runG g) (GithubConfig t)
+runGithub :: B.ByteString  -- ^ Authentication token
+          -> String        -- ^ Base user to consider
+          -> String        -- ^ Base repo
+          -> Github a      -- ^ The Github action to run
+          -> IO a
+runGithub token user repo g = runReaderT (runG g) (GithubConfig token user repo)
 
 -- | Run a get request to a github endpoint in the Github monad
 get :: [String] -> Github L.ByteString
@@ -33,14 +38,26 @@ post :: Postable a => [String] -> a -> Github L.ByteString
 post url p = request postWith' url
   where postWith' opts url = W.postWith opts url p
 
+-- | Get a url relative to the repo
+getRepo :: [String] -> Github L.ByteString
+getRepo = repoUrl >=> get
+
+-- | Post to a url relative to the repo
+postRepo :: Postable a => [String] -> a -> Github L.ByteString
+postRepo url p = do url' <- repoUrl url
+                    post url' p
+
+-- | Constructs an url where the rooted at the repo api endpoint
+repoUrl :: [String] -> Github [String]
+repoUrl url = do config <- Github ask
+                 return $ "repos":(gUser config):(gRepo config):url
 
 type RequestF = W.Options -> String -> IO (W.Response L.ByteString)
 
 githubUrl = "https://api.github.com"
 
 request :: RequestF -> [String] -> Github L.ByteString
-request method components = Github g
-  where g = do
+request method components = Github $ do
           let url = intercalate "/" (githubUrl:components)
           config <- ask
           r <- liftIO $ method (options $ gToken config) url
--- approvd-haskell/Main.hs
+++ approvd-haskell/Main.hs
@@ -9,7 +9,7 @@ import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy.Char8 as L
 import qualified Data.Text as T
 
-import Github (Github, runGithub, get, post)
+import Github (Github, runGithub, getRepo, postRepo)
 
 auth :: IO B.ByteString
 auth = B.init <$> B.readFile "../token"
@@ -18,44 +18,42 @@ context :: String
 context = "approvd-haskell-proto"
 
 -- | Return the comments of an issue
-issueComments :: String -> String -> Int -> Github L.ByteString
-issueComments user repo issue =
-  get ["repos", user, repo, "issues", show issue, "comments"]
+issueComments :: Int -> Github L.ByteString
+issueComments issue = getRepo ["issues", show issue, "comments"]
 
 -- | Return a pull request
-pullRequest :: String -> String -> Int -> Github L.ByteString
-pullRequest user repo pr =
-  get ["repos", user, repo, "pulls", show pr]
+pullRequest :: Int -> Github L.ByteString
+pullRequest pr = getRepo ["pulls", show pr]
 
 -- | Return the status we previously gave to a commit
-ourStatus :: String -> String -> String -> Github (Maybe Value)
-ourStatus user repo sha = do
-  status <- get ["repos", user, repo, "status", sha]
+ourStatus :: String -> Github (Maybe Value)
+ourStatus sha = do
+  status <- getRepo ["status", sha]
   return $ find ours (status ^. key "statuses" . _Array)
     where ours s = s ^. key "context" . _String == T.pack context
 
 -- | Changes the status of a commit
-newStatus
-  :: String -> String -> String -> String -> String -> Github L.ByteString
-newStatus user repo sha state description =
-  post ["repos", user, repo, "statuses", sha] $
-    object [ "state" .= state
-           , "description" .= description
-           , "context" .= context ]
+newStatus :: String               -- | Sha of the commit
+          -> String               -- | State
+          -> String               -- | Description
+          -> Github L.ByteString
+newStatus sha state description =
+  postRepo ["statuses", sha] $ object [ "state" .= state
+                                      , "description" .= description
+                                      , "context" .= context ]
 
 -- | Do stuff with a pull request.  Honesly, we just do random stuff
 -- to check the rest work. Here, for example, we set the status of the
 -- last commit of the pull request to pending if it's not already set
-handlePR :: String -> String -> Int -> Github String
-handlePR user repo pr =
-  do pull <- pullRequest user repo pr
+handlePR :: Int -> Github String
+handlePR pr =
+  do pull <- pullRequest pr
      let sha = T.unpack $ pull ^. key "head" . key "sha" . _String
-     status <- ourStatus user repo sha
+     status <- ourStatus sha
      case status of
       Just s -> return $ T.unpack $ s ^. key "state" . _String
-      Nothing -> L.unpack <$> newStatus user repo sha "pending" "Haskell approves too"
-
+      Nothing -> L.unpack <$> newStatus sha "pending" "Haskell approves too"
 
 main = do token <- auth
-          r <- runGithub token $ handlePR "madjar" "approvd-protos" 1
+          r <- runGithub token "madjar" "approvd-protos" $ handlePR  1
           putStrLn r
