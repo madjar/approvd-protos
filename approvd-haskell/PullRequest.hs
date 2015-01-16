25de0f1935e5377f09704ac66f82f5287162be7e
Georges Dubus <georges.dubus@compiletoi.net>
2015-01-16 18:04:29 +0100

    Skeleton for the commit push date retrieval

--- approvd-haskell/PullRequest.hs
+++ approvd-haskell/PullRequest.hs
@@ -9,7 +9,7 @@ import Data.Foldable (find)
 import qualified Data.ByteString.Lazy.Char8 as L
 import qualified Data.Text as T
 
-import Github (Github, getRepo, postRepo)
+import Github (Github, get, getRepo, postRepo)
 
 import Debug.Trace
 import Control.Lens ((^?))
@@ -29,15 +29,17 @@ issueComments issue = getRepo ["issues", show issue, "comments"]
 pullRequest :: Int -> Github PullRequest
 pullRequest prId = do
   pr <- getRepo ["pulls", show prId]
-  let sha = T.unpack $ pr ^. key "head" . key "sha" . _String
-      -- TODO use the event API to find the push date of the commit (not the repo)
-      --push_date =  pr ^. key "head" . key "repo" . key "pushed_at" . _String -- TODO parse as date
-      push_date = ""
-      relevant issue = (traceShowId $ issue ^. key "created_at" . _String) > push_date
+  let sha = pr ^. key "head" . key "sha" . _String
+  -- TODO parse as date
+  pushDate <- commitPushDate sha
+  let relevant issue = traceShowId $ issue ^. key "created_at" . _String > pushDate
       relevantComments = do comments <- issueComments prId
                             return $ mfilter relevant (comments ^. _Array)
-  return $ PullRequest pr sha relevantComments
+  return $ PullRequest pr (T.unpack sha) relevantComments
 
+-- TODO use the event API to find the push date of the commit (not the repo)
+commitPushDate :: T.Text -> Github T.Text
+commitPushDate = undefined
 
 -- | Return the status we previously gave to a commit
 ourStatus :: String -> Github (Maybe Value)
